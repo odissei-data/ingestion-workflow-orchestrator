fc20e60191c0b46f065d358fdd392a6d1bbd7aaa
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MBP.ia.knaw.local>
2023-01-04 13:33:39 +0100

    WIP - License now parsed for ingestion back into dataverse.

--- prefect_docker/scripts/main_dataverse_nl_ingestion.py
+++ prefect_docker/scripts/main_dataverse_nl_ingestion.py
@@ -8,8 +8,11 @@ DATAVERSE_NL_METADATA_DIRECTORY = os.getenv('DATAVERSE_NL_METADATA_DIRECTORY')
 
 @flow
 def dataverse_nl_ingestion_pipeline():
-    utils.workflow_executor(dataverse_nl_metadata_ingestion,
-                            DATAVERSE_NL_METADATA_DIRECTORY)
+    for sub_dir in os.listdir(DATAVERSE_NL_METADATA_DIRECTORY):
+        metadata_dir = os.path.join(DATAVERSE_NL_METADATA_DIRECTORY, sub_dir)
+        if os.path.isdir(metadata_dir):
+            utils.workflow_executor(dataverse_nl_metadata_ingestion,
+                                    metadata_dir)
 
 
 if __name__ == "__main__":
