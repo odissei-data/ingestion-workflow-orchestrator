7eb1b4e12fac6d790cbf8557d61886f1bfce791d
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MacBook-Pro.local>
2023-01-26 12:03:04 +0100

    Made directories for separating the workflows. Added workflows for all dataverseNL subverses.

--- prefect_docker/scripts/flows/entry_workflows/main_dataverse_nl_ingestion.py
+++ prefect_docker/scripts/flows/entry_workflows/main_dataverse_nl_ingestion.py
@@ -14,11 +14,9 @@ def dataverse_nl_ingestion_pipeline():
                                                    fetcher=True,
                                                    importer=True,
                                                    updater=True)
-    for sub_dir in os.listdir(DATAVERSE_NL_METADATA_DIRECTORY):
-        metadata_dir = os.path.join(DATAVERSE_NL_METADATA_DIRECTORY, sub_dir)
-        if os.path.isdir(metadata_dir):
-            utils.workflow_executor(dataverse_nl_metadata_ingestion,
-                                    metadata_dir, version)
+
+    utils.workflow_executor(dataverse_nl_metadata_ingestion,
+                            DATAVERSE_NL_METADATA_DIRECTORY, version)
 
 
 if __name__ == "__main__":
