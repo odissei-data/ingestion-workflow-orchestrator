2b1b40b783ed625d6d4baa862928c6c8882e961e
Michiel Zuidema <michiel.zuidema@dans.knaw.nl>
2023-02-23 16:28:32 +0100

    Update dataverse.nl ingestion.

--- prefect_docker/scripts/flows/entry_workflows/main_dataverse_nl_ingestion.py
+++ prefect_docker/scripts/flows/entry_workflows/main_dataverse_nl_ingestion.py
@@ -1,23 +1,42 @@
-import os
-import utils
 from prefect import flow
-from flows.dataset_workflows.dataverse_nl_ingestion import dataverse_nl_metadata_ingestion
+
+from flows.dataset_workflows.dataverse_nl_ingestion import \
+    dataverse_nl_metadata_ingestion
 from flows.workflow_versioning.workflow_versioner import \
     create_ingestion_workflow_versioning
-
-DATAVERSE_NL_METADATA_DIRECTORY = os.getenv('DATAVERSE_NL_METADATA_DIRECTORY')
-DATAVERSE_NL_DATAVERSE_ALIAS = os.getenv('DATAVERSE_NL_DATAVERSE_ALIAS')
+import utils
 
 
 @flow
-def dataverse_nl_ingestion_pipeline():
-    version = create_ingestion_workflow_versioning(transformer=True,
-                                                   fetcher=True,
-                                                   importer=True,
-                                                   updater=True)
+def dataverse_nl_ingestion_pipeline(
+        source_dataverse_url,
+        metadata_directory,
+        destination_dataverse_alias
+):
+    """
+    Ingestion pipeline dedicated to dataverse.nl workflow.
 
-    utils.workflow_executor(dataverse_nl_metadata_ingestion,
-                            DATAVERSE_NL_METADATA_DIRECTORY, version)
+    :param source_dataverse_url: string, url to source dataverse
+    :param metadata_directory: string, name of metadata directory
+    :param destination_dataverse_alias: string, alias used by destination
+    dataverse
+
+    :return: None
+    """
+    version = create_ingestion_workflow_versioning(
+        transformer=True,
+        fetcher=True,
+        importer=True,
+        updater=True
+    )
+
+    utils.workflow_executor(
+        data_provider_workflow=dataverse_nl_metadata_ingestion,
+        metadata_directory=metadata_directory,
+        version=version,
+        alias=destination_dataverse_alias,
+        source_dataverse_url=source_dataverse_url
+    )
 
 
 if __name__ == "__main__":
