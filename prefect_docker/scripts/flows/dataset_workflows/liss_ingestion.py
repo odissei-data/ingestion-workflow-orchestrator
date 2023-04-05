dbb701439b6b6c5b9a1fcb5d587b160a10c5bc94
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MacBook-Pro.local>
2023-04-05 14:27:52 +0200

    Changed setup for liss to work with minio.

--- prefect_docker/scripts/flows/dataset_workflows/liss_ingestion.py
+++ prefect_docker/scripts/flows/dataset_workflows/liss_ingestion.py
@@ -57,4 +57,4 @@ def liss_metadata_ingestion(file_path, version, settings_dict):
         if not pub_date_response:
             return Failed(message='Unable to update publication date')
 
-    return Completed(message=file_path + " ingested successfully")
+    return Completed(message=doi + " ingested successfully")
