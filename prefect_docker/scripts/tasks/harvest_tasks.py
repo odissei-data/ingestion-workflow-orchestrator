e505c4f561d22a9f90d92a35155cf18ff17311ef
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MacBook-Pro.local>
2023-08-17 16:47:42 +0200

    Created task for harvesting

---
+++ prefect_docker/scripts/tasks/harvest_tasks.py
@@ -0,0 +1,39 @@
+import json
+
+import requests
+from prefect import task
+
+from configuration.config import settings
+
+
+@task(timeout_seconds=300, retries=1)
+def harvest_metadata(metadata_prefix, oai_endpoint, bucket_name, verb,
+                     harvester_endpoint, oai_set=None):
+    headers = {
+        'Content-Type': 'application/json',
+        'accept': 'application/json'
+    }
+
+    data = {
+        "metadata_prefix": metadata_prefix,
+        "oai_endpoint": oai_endpoint,
+        "bucket_name": bucket_name,
+        "verb": verb
+    }
+
+    print(f'data: {data}')
+
+    if oai_set is not None:
+        data['oai_set'] = oai_set
+
+    url = f"{settings.HARVESTER_URL}/{harvester_endpoint}"
+    print(f'url: {url}')
+
+    response = requests.post(
+        url, headers=headers, data=json.dumps(data)
+    )
+
+    if not response.ok:
+        raise Exception(
+            f'Request failed with status code {response.status_code}: {response.text}')
+
