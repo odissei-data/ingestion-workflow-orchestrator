97e880469963898e7f91b51452edb46d2fdbc2c7
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MBP.ia.knaw.local>
2024-03-27 17:02:34 +0100

    `Added changes to code necessary for poetry v16.

--- scripts/deployment/deploy_ingestion_pipelines.py
+++ scripts/deployment/deploy_ingestion_pipelines.py
@@ -1,24 +1,18 @@
-from prefect.deployments import Deployment
+from prefect import serve
 from flows.entry_workflows.main_cbs_ingestion import cbs_ingestion_pipeline
 from flows.entry_workflows.main_cid_ingestion import cid_ingestion_pipeline
 from flows.entry_workflows.main_liss_ingestion import liss_ingestion_pipeline
 from flows.entry_workflows.main_dataverse_ingestion import \
     dataverse_ingestion_pipeline
 
-
-def build_deployment(workflow_function, name):
-    deployment = Deployment.build_from_flow(
-        name=name,
-        flow_name=name,
-        flow=workflow_function,
-        work_queue_name='default',
-        load_existing=True
-    )
-    deployment.apply()
-
-
 if __name__ == "__main__":
-    build_deployment(cbs_ingestion_pipeline, "cbs-ingestion")
-    build_deployment(cid_ingestion_pipeline, "cid-ingestion")
-    build_deployment(dataverse_ingestion_pipeline, "dataverse-ingestion")
-    build_deployment(liss_ingestion_pipeline, "liss-ingestion")
+    cbs_deploy = cbs_ingestion_pipeline.to_deployment(
+        name="cbs-ingestion-deployment")
+    cid_deploy = cid_ingestion_pipeline.to_deployment(
+        name="cid-ingestion-deployment")
+    liss_deploy = liss_ingestion_pipeline.to_deployment(
+        name="liss-ingestion-deployment")
+    dv_deploy = dataverse_ingestion_pipeline.to_deployment(
+        name="dataverse-ingestion-deployment")
+
+    serve(cbs_deploy, cid_deploy, liss_deploy, dv_deploy)
