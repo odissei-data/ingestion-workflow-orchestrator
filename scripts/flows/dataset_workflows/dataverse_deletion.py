ba311e5dd4cc0afd974d2644d355409f38bbd4b3
ekoi <eko.indarto@dans.knaw.nl>
2024-10-01 10:29:23 +0200

    Add deletion pipeline dedicated to the Dataverse to Dataverse workflow

---
+++ scripts/flows/dataset_workflows/dataverse_deletion.py
@@ -0,0 +1,34 @@
+from prefect import flow
+from prefect.server.schemas.states import Failed, Completed
+
+from tasks.base_tasks import dataverse_dataset_check_status, \
+    delete_dataset
+from utils import generate_dv_flow_run_name, failed_dataverse_deletion_hook
+
+
+@flow(name="Deleting Dataverse metadata", flow_run_name= generate_dv_flow_run_name,
+      on_failure=[failed_dataverse_deletion_hook])
+def dataverse_metadata_deletion(pid, settings_dict):
+    """
+    Deletion flow for Dataverse to dataverse ingestion.
+
+    :param pid: pid of the dataset.
+    :param settings_dict: dict, contains settings for the current workflow.
+    :return: prefect.server.schemas.states Failed or Completed.
+    """
+    dv_response_status = dataverse_dataset_check_status(pid, settings_dict)
+    # Dataset exists: 200
+    # Dataset does not exist: 404
+    # Dataset deaccession: 403
+    if not dv_response_status:
+        return Failed(message=f'No response from {pid}.')
+
+    if dv_response_status in (404, 403):
+        response = delete_dataset(pid, settings_dict)
+        if not response:
+            return Failed(message=f'Unable to delete dataset: {pid}.')
+
+        return Completed(message=pid + 'deleted successfully.')
+
+    # Dataset is active: 200
+    return Failed(message=f'{pid} will not deleted. Dataset is active.')
