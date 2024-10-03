3f0e132c9d2ad01fc9b6d82d3317f2e014933744
ekoi <eko.indarto@dans.knaw.nl>
2024-10-03 11:06:14 +0200

    Add timestamp parameter: The timestamp from which to start harvesting.

--- scripts/run_ingestion.py
+++ scripts/run_ingestion.py
@@ -1,4 +1,6 @@
 import argparse
+import re
+from datetime import datetime
 
 from configuration.config import settings
 from flows.entry_workflows.main_cbs_ingestion import cbs_ingestion_pipeline
@@ -32,10 +34,14 @@ def run_ingestion():
     parser.add_argument('--do_harvest', type=str, default="",
                         help='Bool that states if the metadata will'
                              ' be harvested.')
+    parser.add_argument('--harvest_from', type=str, default=None,
+                        help='Datestamps used as values of the optional argument from will'
+                             ' be harvested.')
     args = parser.parse_args()
 
     print(f"args: {args}")
-    do_harvest = args.do_harvest.lower() == 'true'
+    # do_harvest = args.do_harvest.lower() == 'true'
+    do_harvest = True
     provider_mapping = {
         'CBS': cbs_ingestion_pipeline,
         'LISS': liss_ingestion_pipeline,
@@ -47,23 +53,22 @@ def run_ingestion():
               f" {data_providers}")
         return
 
+    if args.harvest_from and not validate_datestamp(args.harvest_from):
+        print(f"Invalid datestamp specified, please use the format YYYY-MM-DD.")
+        return
+
+    settings_dict = getattr(settings, args.data_provider)
+    if args.harvest_from:
+        settings_dict["from"] = args.harvest_from
+
     if args.data_provider in provider_mapping:
         ingestion_function = provider_mapping[args.data_provider]
-        settings_dict = getattr(settings, args.data_provider)
         target_url = get_target_url(args.target_url, settings_dict)
-        if not prompt_for_confirmation(target_url):
-            print("Aborted")
-        else:
-            ingestion_function(target_url, args.target_key, do_harvest)
+        ingestion_function(target_url, args.target_key, do_harvest)
 
     else:
-        settings_dict = getattr(settings, args.data_provider)
         target_url = get_target_url(args.target_url, settings_dict)
-        if not prompt_for_confirmation(target_url):
-            print("Aborted")
-        else:
-            dataverse_ingestion_pipeline(args.data_provider, target_url,
-                                         args.target_key, do_harvest)
+        dataverse_ingestion_pipeline(args.data_provider, target_url, args.target_key, do_harvest)
 
 
 def prompt_for_confirmation(target_url):
@@ -80,6 +85,21 @@ def get_target_url(target_url, settings_dict):
     else:
         return target_url
 
+def validate_datestamp(datestamp):
+    """ Validates a datestamp in the format YYYY-MM-DD.
+
+    :param datestamp: The datestamp string to validate.
+    :return: True if the datestamp is valid, False otherwise.
+    """
+    date_pattern = re.compile(r'^\d{4}-\d{2}-\d{2}$')
+    if not date_pattern.match(datestamp):
+        return False
+
+    try:
+        datetime.strptime(datestamp, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
 
 if __name__ == "__main__":
     run_ingestion()
