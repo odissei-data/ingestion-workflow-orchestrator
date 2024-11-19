ce13d1337b81f6ac24ccf37111a2f0a051a7d1a5
ekoi <eko.indarto@dans.knaw.nl>
2024-11-19 13:01:10 +0100

    Intermediate commit: cid using oai-pmh endpoint

--- scripts/tasks/base_tasks.py
+++ scripts/tasks/base_tasks.py
@@ -37,6 +37,34 @@ def xml2json(xml_metadata):
 
     return response.json()
 
+@task(timeout_seconds=300, retries=1, cache_expiration=timedelta(minutes=10))
+def xml2dvjson(xml_metadata):
+    """ Sends XML to the transformer server, receives JSON with same hierarchy.
+
+    Sends a request to the transformer endpoint for transformation
+    from xml to json. Needs an authorization token to use the transformer API.
+
+    :param xml_metadata: The XML contents
+    :return: Plain JSON metadata | None on failure.
+    """
+    logger = get_run_logger()
+
+    headers = {
+        'Content-Type': 'application/xml',
+        'Authorization': settings.XML2JSON_API_TOKEN,
+    }
+
+    url = f"{settings.DANS_TRANSFORMER_SERVICE}/transform/{settings.XSLT_TRANSFORMER_NAME}"
+    response = requests.post(
+        url,
+        headers=headers, data=xml_metadata
+    )
+
+    if not response.ok:
+        logger.info(response.text)
+        return None
+
+    return response.json()['result']
 
 @task(timeout_seconds=300, retries=1, cache_expiration=timedelta(minutes=10))
 def dataverse_mapper(json_metadata, mapping_file_path, template_file_path,
