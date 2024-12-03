41d6c4d45369ab41021827e4fc5459f478302ad3
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MBP.ia.knaw.local>
2024-12-03 15:56:01 +0100

    Upgraded semantic enrichment workflow, now takes a data provider as argument instead of using hardcoded version.

--- scripts/flows/entry_workflows/enrichment.py
+++ scripts/flows/entry_workflows/enrichment.py
@@ -1,3 +1,5 @@
+import argparse
+
 from prefect import flow
 
 from configuration.config import settings
@@ -6,13 +8,19 @@ from tasks.base_tasks import extract_doi_from_dataverse
 
 
 @flow
-def enrichment_ingestion_pipeline():
-    settings_dict = settings.CBS
+def enrichment_ingestion_pipeline(settings_dict_name: str):
+    settings_dict = getattr(settings, settings_dict_name)
 
     dois = extract_doi_from_dataverse(settings_dict, settings_dict.ALIAS)
 
     for doi in dois:
         semantic_enrichment(settings_dict, doi)
 
+
 if __name__ == "__main__":
-    enrichment_ingestion_pipeline()
+    parser = argparse.ArgumentParser(
+        description="Run the enrichment pipeline with specified settings.")
+    parser.add_argument("settings_dict_name", type=str,
+                        help="Name of the settings dictionary to use.")
+    args = parser.parse_args()
+    enrichment_ingestion_pipeline(args.settings_dict_name)
